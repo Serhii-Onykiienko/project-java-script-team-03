558954c1bbd33670bc3e57dce9e420ddcb85ffde
Big EL <onikienkosergey@gmail.com>
2026-07-01 21:55:07 +0300

    Configure Vite build and deploy

---
+++ src/main.js
@@ -0,0 +1,8 @@
+import './js/header';
+import './js/mobile-menu';
+import './js/feedbacks';
+import './js/portfolio';
+import './js/faq';
+import './js/contacts';
+import './js/footer';
+import './js/success-modal';
